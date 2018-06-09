9f64347c94374cc98331ca1ba2497fd1dbe1d9b4
Ajna Zatric <azatric1@etf.unsa.ba>
2018-06-09 20:23:06 +0200

    Uradila implementaciju eksternog uređaja.Na stranici Admin.xaml.cs prilikom registracije uposlenika, implementirana mogućnost unosa slike uposlenika, sa automatskim paljenjem kamere i mogućnošću slikanja

--- Projekat/AvMauAzil/AvMauAzil/Views/AdminPage.xaml.cs
+++ Projekat/AvMauAzil/AvMauAzil/Views/AdminPage.xaml.cs
@@ -14,6 +14,21 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using AvMauAzil.Models;
 
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.Storage.Pickers;
+using Windows.Storage;
+using Windows.UI.Popups;
+using Windows.Storage.Streams;
+//using AvMauAzil.Views;
+using Microsoft.WindowsAzure.MobileServices;
+using Windows.Media.Capture;
+using Windows.Storage;
+using Windows.Media.Capture;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.Graphics.Imaging;
+using Windows.UI.Xaml.Media.Imaging;
+
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace AvMauAzil
@@ -43,10 +58,50 @@ namespace AvMauAzil
         {
 
         }
+        private async void Uslikaj_kamerom(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //
+                var UslikajUI = new CameraCaptureUI();
+                UslikajUI.PhotoSettings.Format = CameraCaptureUIPhotoFormat.Jpeg;
+                UslikajUI.PhotoSettings.CroppedSizeInPixels = new Size(200, 200);
+                StorageFile slika = await UslikajUI.CaptureFileAsync(CameraCaptureUIMode.Photo);
 
-        private void btnRegistriraj_Click_1(object sender, RoutedEventArgs e)
+
+                IRandomAccessStream stream = await slika.OpenAsync(FileAccessMode.Read);
+                BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
+                SoftwareBitmap softwareBitmap = await decoder.GetSoftwareBitmapAsync();
+
+                SoftwareBitmap softwareBitmapBGR8 = SoftwareBitmap.Convert(softwareBitmap,
+                BitmapPixelFormat.Bgra8,
+                BitmapAlphaMode.Premultiplied);
+
+                SoftwareBitmapSource bitmapSource = new SoftwareBitmapSource();
+                await bitmapSource.SetBitmapAsync(softwareBitmapBGR8);
+
+                polje_za_sliku.Source = bitmapSource;
+            }
+            catch { }
+
+        }
+       /* private void UslikajButton_Click(object sender, RoutedEventArgs e)
+        {
+            Uslikaj_kamerom(sender, e);
+        }*/
+            private void btnRegistriraj_Click_1(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void myListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btn_UnesiSliku_Click(object sender, RoutedEventArgs e)
+        {
+            Uslikaj_kamerom(sender, e);
         }
     }
 }
