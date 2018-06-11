4a68af1f0c3eaaad83f91e293ea59230ff6cf43c
Ajna Zatric <azatric1@etf.unsa.ba>
2018-06-11 23:20:59 +0200

    Dodata prezentacija i videi

---
+++ Projekat/AvMauAzil/AvMauAzil/Models/LoadBalancer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvMauAzil.Models
+{
+    class LoadBalancer
+    {
+        private static LoadBalancer _instance;
+        private List<string> _servers = new List<string>();
+        private Random _random = new Random();
+      
+        private static object syncLock = new object();
+        // Konstruktor zaštićeni
+        protected LoadBalancer()
+        {
+            // Lista dostupnih servera
+            _servers.Add("ServerI");
+            _servers.Add("ServerII");
+            _servers.Add("ServerIII");
+            _servers.Add("ServerIV");
+            _servers.Add("ServerV");
+        }
+        public static LoadBalancer GetLoadBalancer()
+        {
+
+            if (_instance == null)
+            {
+                lock (syncLock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new LoadBalancer();
+                    }
+                }
+            }
+            return _instance;
+        }
+       
+        public string Server
+        {
+            get
+            {
+                int r = _random.Next(_servers.Count);
+                return _servers[r].ToString();
+            }
+        }
+    }
+}
+    
